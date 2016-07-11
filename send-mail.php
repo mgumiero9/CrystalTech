ef5c2cca616b8648d80f417301a3eb6716b29c14
mgumiero9 <mgumiero9@gmail.com>
2016-07-11 09:36:37 -0300

    fixed send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -1,23 +1,19 @@
 
 
 <?
+$body = "";
 
 $name=$_POST['name'];
 $Email=$_POST['email'];
-$website=$_POST['url'];
 $message=$_POST['message'];
 
-
-    
     $body .= "Name: " . $name . "\n"; 
     $body .= "Email: " . $Email . "\n"; 
-    $body .= "Website: " . $website . "\n"; 
-    $body .= "Message: " . $message . "\n"; 
+    $body .= "Message: " . $message . "\n";
 
     //replace with your email
     mail("mgumiero9@gmail.com","CrystalTech New email",$body); 
 
-  
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
